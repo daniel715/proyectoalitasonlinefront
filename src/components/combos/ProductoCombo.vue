6712feb4546bb4684d0dde9da5c79d74fa7a5eb4
Daniel Montellanos <daniel.montellanos.contractor@bbva.com>
2023-07-07 14:00:02 -0500

    agregando componente ProductoCombo

---
+++ src/components/combos/ProductoCombo.vue
@@ -0,0 +1,87 @@
+<template>
+  <v-row>
+    <v-col>
+      <table>
+        <thead>
+          <th>Producto</th>
+          <th>Precio</th>
+          <th>Cantidad</th>
+          <th>Total</th>
+          <th></th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <v-combobox
+                v-model="productoSeleccionado"
+                :items="productos"
+                variant="solo-inverted"
+                item-text="nombre"
+                item-value="productoId"
+              ></v-combobox>
+            </td>
+            <td>{{ productoSeleccionado.precio }}</td>
+            <td>
+              <v-text-field type="number" v-model="cantidad" label=""></v-text-field>
+            </td>
+            <td>{{ productoSeleccionado.precio * cantidad }}</td>
+            <td>
+              <v-btn @click="addItem" small icon>
+                <v-icon>mdi-plus</v-icon>
+              </v-btn>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </v-col>
+    <v-col>
+      <span v-for="(item, index) in pedidosArray" :key="index">
+        {{ item.nombre }}
+        {{ item.precio }}
+        {{ item.cantidad }}
+        {{ item.precio * item.cantidad }}
+      </span>
+    </v-col>
+  </v-row>
+</template>
+<script>
+import { defineComponent } from '@vue/composition-api'
+import { mapGetters } from 'vuex'
+export default defineComponent({
+  data: () => ({
+    productoSeleccionado: {
+      nombre: '',
+      precio: '',
+    },
+    cantidad: '',
+    productos: [],
+    pedidoproductos: [],
+    pedidosArray: [],
+  }),
+  computed: {
+    ...mapGetters(['allProductos', 'allPedidosProductos']),
+  },
+  methods: {
+    addItem() {
+      this.productoSeleccionado.cantidad = this.cantidad
+      this.pedidosArray.push(this.productoSeleccionado)
+    },
+    // setItems() {
+    //   this.productos.forEach((producto) => {
+    //     this.pedidoproductos.forEach((element) => {
+    //       if (producto.productoId == element.productoId) {
+    //         console.log(element)
+    //         producto['cantidad'] = element.precio
+    //       }
+    //     })
+    //   })
+    // },
+  },
+  created() {
+    this.productos = this.allProductos
+    this.pedidoproductos = this.allPedidosProductos
+    // this.setItems()
+    console.log(this.productos)
+  },
+})
+</script>
